3bf6233c8c69e5fcbc3933ea4c0a5499effccd6f
chowhr1231 <thddbswo14@gmail.com>
2019-09-03 23:18:40 +0900

    [ADD] setting
    local일 때는 MySQL DB에 접근 test일 때는 h2 console에 접근

--- src/main/java/com/chatbot/application/Application.java
+++ src/main/java/com/chatbot/application/Application.java
@@ -2,12 +2,19 @@ package com.chatbot.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
+    private static final String APPLICATION_LOCATION = "spring.config.location=" +
+            "classpath:application.yml," +
+            "classpath:application/db_application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATION)
+                .run(args);
     }
 
 }
