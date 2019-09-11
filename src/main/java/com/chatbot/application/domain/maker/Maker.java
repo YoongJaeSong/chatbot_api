6214363c8ffb7d76d064b3f7054ab26ddc46210f
chowhr1231 <thddbswo14@gmail.com>
2019-09-11 14:42:40 +0900

    Maker Entity를 추가

---
+++ src/main/java/com/chatbot/application/domain/maker/Maker.java
@@ -0,0 +1,34 @@
+package com.chatbot.application.domain.maker;
+
+import com.chatbot.application.domain.car.Car;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Maker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private int no;
+
+    @Column(length = 50)
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "maker_id")
+    private List<Car> cars = new ArrayList<>();
+
+    @Builder
+    public Maker(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
+}
