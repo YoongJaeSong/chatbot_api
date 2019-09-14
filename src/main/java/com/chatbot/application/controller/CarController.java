f3b75b32aa0f8c5d9675c96fd7c15fd7537d5db6
chowhr1231 <thddbswo14@gmail.com>
2019-09-15 00:00:07 +0900

    Car controller와 service에 메서드 추가
    Maker별로 차량 조회하는 메서드 추가

--- src/main/java/com/chatbot/application/controller/CarController.java
+++ src/main/java/com/chatbot/application/controller/CarController.java
@@ -1,9 +1,23 @@
 package com.chatbot.application.controller;
 
+import com.chatbot.application.domain.car.Car;
+import com.chatbot.application.service.CarService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@AllArgsConstructor
 public class CarController {
 
+    private CarService service;
+
+    @GetMapping("/api/cars/{makerId}")
+    public List<Car> findCarsByMakerId(@PathVariable long makerId){
+        return service.getCarsByMakerId(makerId);
+    }
 
 }
