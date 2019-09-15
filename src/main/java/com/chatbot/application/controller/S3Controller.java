9f26aefc0d4a1a92ec2a00b32ca25de8b19923fe
chowhr1231 <thddbswo14@gmail.com>
2019-09-15 15:49:27 +0900

    controller에 s3에 업로드 api 추가
    post method를 사용하며 multipart form data로 변수명은 image로 받는다.

--- src/main/java/com/chatbot/application/controller/S3Controller.java
+++ src/main/java/com/chatbot/application/controller/S3Controller.java
@@ -1,4 +1,21 @@
 package com.chatbot.application.controller;
 
+import com.chatbot.application.aws.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
 public class S3Controller {
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/api/image/upload")
+    @ResponseBody
+    public String uploadToS3(@RequestParam("image") MultipartFile multipartFile) throws IOException {
+        return s3Uploader.imageUpload(multipartFile, "static");
+    }
 }
