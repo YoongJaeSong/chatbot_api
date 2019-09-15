7139579397c28b0a2a35ee70f1c6feb4542cc1cd
chowhr1231 <thddbswo14@gmail.com>
2019-09-15 14:24:42 +0900

    S3 upload 메서드 추가
    https://jojoldu.tistory.com/300을 그대로 참고해 만듦

--- src/main/java/com/chatbot/application/aws/S3Uploader.java
+++ src/main/java/com/chatbot/application/aws/S3Uploader.java
@@ -1,4 +1,70 @@
 package com.chatbot.application.aws;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class S3Uploader {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String imageUpload(MultipartFile file, String dirName) throws IOException {
+        File uploadFile = convert(file).orElseThrow(
+                () -> new IllegalArgumentException("MultipartFile -> File 전환 실패")
+        );
+
+        return uploadToS3(uploadFile, dirName);
+    }
+
+    private String uploadToS3(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile);
+        return uploadUrl;
+    }
+
+    private void removeNewFile(File targetFile) {
+        if(targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        }else{
+            log.info("파일을 삭제하지 못했습니다.");
+        }
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+    }
 }
